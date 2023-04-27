b1a41ae9710f948a0c29f8ec8806d0a68a7889c6
THIAGO SANTANA DOS SANTOS CONCEICAO <thiago.ssconceicao@senacsp.edu.br>
2023-04-27 17:08:18 -0300

    atualização

---
+++ src/routes/index.ts
@@ -0,0 +1,29 @@
+import {Router, Request, Response} from 'express'
+import * as pageController from '../controllers/pageController'
+import * as userController from '../controllers/userController'
+
+const router = Router()
+
+router.get('/',pageController.home)
+
+router.get('/mapa',pageController.mapa)
+
+router.get('/faleconosco', pageController.faleconosco)
+
+router.get('/sobrenos',pageController.sobrenos)
+ 
+router.get('/usuario',pageController.usuario)
+
+router.get('/cadastro', userController.cadastro)
+
+router.get('/login',userController.login)
+
+router.post('/localizacao',userController.salvaLocalizacao)
+
+
+
+
+
+
+
+export default router
