2d6278be4c2528678ff3d5fbeffb0120f4283e1d
THIAGO SANTANA DOS SANTOS CONCEICAO <thiago.ssconceicao@senacsp.edu.br>
2023-05-23 17:17:22 -0300

    att

--- public/js/header.js
+++ public/js/header.js
@@ -3,10 +3,10 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "images/GuiH&F/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "images/Header&Footer/menu_white_36dp.svg";
     } 
     else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "images/GuiH&F/close_white_36dp.svg";
+        document.querySelector('.icon').src = "images/Header&Footer/close_white_36dp.svg";
     }
 }
